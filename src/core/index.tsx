ac7c4d8f2f36e79efd553274faf9133014a7a6b0
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2022-11-07 17:40:04 -0300

    feat: added theme and fonts

---
+++ src/core/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Router from "./router";
+import theme from "@shared/theme";
+import { useFonts } from "expo-font";
+import { ThemeProvider } from "styled-components";
+
+export default function App() {
+  const [loadedFonts] = useFonts({
+    "Montserrat-Regular": require("@assets/fonts/Montserrat-Regular.ttf"),
+    "Montserrat-Medium": require("@assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-Bold": require("@assets/fonts/Montserrat-Bold.ttf"),
+  });
+
+  if (!loadedFonts) return <></>;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
+  );
+}
---
+++ src/shared/theme/index.tsx
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    primary_background: "#F6F6F5",
+    secondary_background: "#FFFFFF",
+    terciary_background: "#000000",
+
+    primary_button: "#1A76D2",
+    secondary_button: "#FFD02C",
+
+    primary_text: "#666666",
+    secondary_text: "#CBCBCB",
+  },
+  fonts: {
+    regular: "Montserrat-Regular",
+    medium: "Montserrat-Medium",
+    bold: "Montserrat-Bold",
+  },
+};
