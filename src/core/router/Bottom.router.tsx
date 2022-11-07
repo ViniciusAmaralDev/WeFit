8007d178afd2e3b08a56c6a54309f5bb5fe57232
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2022-11-07 19:11:51 -0300

    feat: adicionado repository screen and icons

--- src/core/router/Bottom.router.tsx
+++ src/core/router/Bottom.router.tsx
@@ -1,16 +1,36 @@
 import React from "react";
+import { AntDesign, MaterialIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-// import Repositories from "@modules/repositories/screens";
+import Repositories from "@modules/repositories/screens";
+import { useTheme } from "styled-components";
 // import Favorites from "@modules/favorites/screens";
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomRouter() {
+  const theme = useTheme();
+
   return (
-    <Tab.Navigator>
-      {/* <Tab.Screen name="Repositories" component={Repositories} />
-      <Tab.Screen name="Favorites" component={Favorites} /> */}
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelStyle: {
+          fontFamily: theme.fonts.medium,
+          // color: theme.colors.primary_text,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Repositories"
+        component={Repositories}
+        options={{
+          tabBarLabel: "Repositórios",
+          tabBarIcon: ({ size, color }) => (
+            <AntDesign name="github" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
