2e0c4d06d3c83ff6b0e7a318b1253f73e99b262b
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2022-11-07 17:19:37 -0300

    feat: added home module

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@core": "./src/core",
+            "@modules": "./src/modules",
+            "@shared": "./src/shared",
+            "@components": "./src/shared/components",
+            "@assets": "./src/shared/assets",
+          },
+        },
+      ],
+    ],
   };
 };
