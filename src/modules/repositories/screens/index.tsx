846d5006454490898405899278ca548278e0f54b
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2022-11-08 00:03:35 -0300

    feat: added repository list

--- src/modules/repositories/screens/index.tsx
+++ src/modules/repositories/screens/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { Container } from "./styles";
 import { useRepository } from "../hook";
 import { StatusBar } from "expo-status-bar";
 import { Header } from "../components/header";
+import { Container, FlatList } from "./styles";
+import { Card } from "../components/card";
+import { Repository } from "../hook/types";
 
 const Repositories = () => {
-  const { toggleModalOfSelectRepository } = useRepository();
+  const { repositories, toggleModalOfSelectRepository } = useRepository();
 
   return (
     <Container>
       <StatusBar style="dark" />
       <Header title="WeFit" onPress={toggleModalOfSelectRepository} />
+      <FlatList
+        data={repositories}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => <Card repository={item as Repository} />}
+      />
     </Container>
   );
 };
--- src/modules/repositories/screens/styles.ts
+++ src/modules/repositories/screens/styles.ts
@@ -1,6 +1,14 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.primary_background};
 `;
+
+export const FlatList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingHorizontal: RFValue(8),
+  },
+})``;
