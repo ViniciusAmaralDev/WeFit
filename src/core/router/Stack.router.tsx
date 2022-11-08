44411231cc2f7abd2d9bcd5960c222d43df81df8
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2022-11-08 08:21:21 -0300

    refactor: added header

--- src/core/router/Stack.router.tsx
+++ src/core/router/Stack.router.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import BottomRouter from "./Bottom.router";
 import { Details } from "@modules/repositories/screens/details";
+import { DetailScreenHeader } from "@shared/components/detailScreenHeader";
 
 export type StackRootParamList = {
   BottomRouter: undefined;
@@ -14,9 +15,17 @@ const Stack = createStackNavigator<StackRootParamList>();
 
 export default function StackRouter() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="BottomRouter" component={BottomRouter} />
-      <Stack.Screen name="Details" component={Details} />
+    <Stack.Navigator>
+      <Stack.Screen
+        name="BottomRouter"
+        component={BottomRouter}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={Details}
+        options={{ header: () => <DetailScreenHeader /> }}
+      />
     </Stack.Navigator>
   );
 }
