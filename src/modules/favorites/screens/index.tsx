bd93ae8ddf7a8da8f891b0affb8a71e7170ff5d2
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2022-11-08 08:22:02 -0300

    refactor: added repository list

--- src/modules/favorites/screens/index.tsx
+++ src/modules/favorites/screens/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Container } from "./styles";
+import { useRepository } from "@modules/repositories/hook";
+import { RepositoryList } from "@shared/components/repositoriesList";
 
 const Favorites = () => {
+  const { repositories } = useRepository();
+
   return (
     <Container>
-      <></>
+      <RepositoryList
+        repositories={repositories.filter((repository) => repository.favorite)}
+      />
     </Container>
   );
 };
